5a0bc7ff0acdb1f0591a921c73062d8b9792de71
Inna <inna_murzin@yahoo.com>
2022-03-04 22:55:48 +0300

    fix: исправлены ошибки ревью 1

--- src/ru/yandex/practicum/task/logic/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/task/logic/InMemoryHistoryManager.java
@@ -1,16 +1,17 @@
 package ru.yandex.practicum.task.logic;
 
 import ru.yandex.practicum.task.models.Task;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager{
 
-    private static List<Task> historyList = new ArrayList();
+    private static List<Task> historyList = new LinkedList();
+    private static long MAX_HISTORY_LENGTH = 10L;
 
     @Override
     public void add(Task task) {
-        if (historyList.size()>=10){
+        if (historyList.size()>=MAX_HISTORY_LENGTH){
             historyList.remove(0);
         }
         historyList.add(task);
--- src/ru/yandex/practicum/task/logic/Managers.java
+++ src/ru/yandex/practicum/task/logic/Managers.java
@@ -2,21 +2,11 @@ package ru.yandex.practicum.task.logic;
 
 public class Managers {
 
-    private static final String TASK_MANAGER_TYPE ="IN_MEMORY";
-    private static final String HISTORY_MANAGER_TYPE ="IN_MEMORY";
-
-    public static TaskManager getDefault(){
-        if(TASK_MANAGER_TYPE.equalsIgnoreCase("IN_MEMORY"))
-            return new InMemoryTaskManager();
-        //сюда добавляем if else когда у нас появятся другие типы TaskManager
-        return null;
+    public static TaskManager getDefault() {
+        return new InMemoryTaskManager();
     }
-
-    public static HistoryManager getDefaultHistory(){
-        if(HISTORY_MANAGER_TYPE.equalsIgnoreCase("IN_MEMORY"))
-            return new InMemoryHistoryManager();
-        //сюда добавляем if else когда у нас появятся другие типы TaskManager
-        return null;
+    public static HistoryManager getDefaultHistory() {
+        return new InMemoryHistoryManager();
     }
 
 
